67d4583417fee05f87e604db34b95a1918314356
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2020-10-25 11:52:28 -0700

    redo html to look more like example and read easier

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
 var today = moment().format('l');
-$(".today").append(today);
+$(".date").append(today);
 
 // WHEN I scroll down
 // THEN I am presented with time blocks for standard business hours
@@ -11,6 +11,38 @@ $(".today").append(today);
 
 // WHEN I view the time blocks for that day
 // THEN each time block is color - coded to indicate whether it is in the past, present, or future
+// var auditTask = function (taskEl) {
+//   // get time from time element
+//   var date = $(taskEl).find(".time").text().trim();
+//   // ensure it worked
+//   console.log(date);
+
+//   // convert to moment object at 5:00pm
+//   var time = moment(date, "L").set("hour", 17);
+//   // this should print out an object for the value of the date variable, but at 5:00pm of that date
+//   console.log(time);
+
+//   // apply new class if task is near/over due date
+//   if (moment().isAfter(time)) {
+//     $(taskEl).addClass("list-group-item-danger");
+//   }
+
+// };
+
+// check due date
+// auditTask(taskLi);
+
+//var timeBlocks = // array?
+var currentTime = moment().format('LT');
+console.log(currentTime);
+function timeColor() {
+  // remove any old classes from element
+  $(taskEl).removeClass("text-item-bg-light");
+
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("text-item-danger");
+  }
+};
 
 
 
